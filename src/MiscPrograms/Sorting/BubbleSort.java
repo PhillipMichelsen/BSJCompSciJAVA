f0b32db5764effa9b38d8b90a1d8b1befaf7ca91
Phillip Michelsen <pjmichelsen@gmail.com>
2023-01-13 14:33:25 +0700

    Improved efficiency of BubbleSort.java

--- src/MiscPrograms/Sorting/BubbleSort.java
+++ src/MiscPrograms/Sorting/BubbleSort.java
@@ -2,21 +2,25 @@ package MiscPrograms.Sorting;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] array = { 5, 1, 12, -5, 16 };
+        int[] array = { 5, 1, 12, -5, 16, 92, 13, 42, 2, 0 };
         int temp;
+        boolean swapMade;
 
-        for(int currentPass=0; currentPass < array.length; currentPass++){
-            for(int currentIndex=1; currentIndex < (array.length-currentPass); currentIndex++){
-                if(array[currentIndex-1] > array[currentIndex]){
-                    temp = array[currentIndex-1];
-                    array[currentIndex-1] = array[currentIndex];
-                    array[currentIndex] = temp;
+        do {
+            swapMade = false;
+            for(int currentIndex=0; currentIndex < (array.length-1); currentIndex++){
+                if(array[currentIndex] > array[currentIndex+1]){
+                    temp = array[currentIndex];
+                    array[currentIndex] = array[currentIndex+1];
+                    array[currentIndex+1] = temp;
+                    swapMade = true;
                 }
             }
         }
+        while (swapMade);
 
-        for(int i=0; i < array.length; i++){
-            System.out.print(array[i] + " ");
+        for (int j : array) {
+            System.out.print(j + " ");
         }
 
     }
